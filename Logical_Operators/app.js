3551d2f1f48af79e6dc3a344b84947e6f078f171
qchris101 <qchris101@yahoo.com>
2024-02-27 14:50:44 -0500

    Completed Lesson.

--- Logical_Operators/app.js
+++ Logical_Operators/app.js
@@ -1,15 +1,18 @@
 // =====================
 // LOGICAL AND OPERATOR
 // =====================
-const password = prompt("Enter your password");
-if (password.length >= 6 && password.indexOf(' ') === -1) {
-    console.log("VALID PASSWORD!")
-} else {
-    console.log("INCORRECT FORMAT FOR PASSWORD!")
+const password = prompt("Enter Your Password")
+if (password.length >=6 && password.indexOf(" " === -1)) {
+    console.log("Valid Password")
+} else{
+    console.log("INCORRECT FORMAT")
 }
 
 // =====================
 // REVISITING AGE EXAMPLE
+// =====================
+// =====================
+// COMBINING && and ||
 // =====================
 
 // 0-5 free 
@@ -17,29 +20,23 @@ if (password.length >= 6 && password.indexOf(' ') === -1) {
 // 10-65 $20
 // 65+ free
 
-const age = 100;
-if ((age >= 0 && age < 5) || age >= 65) {
-    console.log("FREE");
-} else if (age >= 5 && age < 10) {
+ const age = 90
+ if (age >=0 && age < 5 || age>=65) {
+    console.log("FREE")
+ }else if (age >= 5 && age < 10){
     console.log("$10")
-} else if (age >= 10 && age < 65) {
+ }else if (age >= 10 && age < 65){
     console.log("$20")
-} else {
-    console.log("INVALID AGE!")
-}
+ } else {
+    console.log("INVALID AGE")
+ }
 
-// let firstName = prompt("enter your first name");
-// if (!firstName) {
-//     firstName = prompt("TRY AGAIN!!!");
-// }
 
-// =====================
-// COMBINING && and ||
-// =====================
-const age = 8;
-if (!(age >= 0 && age < 5 || age >= 65)) {
-    console.log("YOU ARE NOT A BABY OR A SENIOR!")
-}
+ let firstName = prompt("enter your first name");
+ if (!firstName) {
+     firstName = prompt("TRY AGAIN!!!");
+ }
+
 
 
 
